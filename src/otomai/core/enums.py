ef292e85f499ddf26fe6c216fe7cad115e459ff4
thomasclem <thomas_cl@gmx.fr>
2025-08-16 16:26:05 -0500

    feat: add OHLCVTimeframe enum for more safety

--- src/otomai/core/enums.py
+++ src/otomai/core/enums.py
@@ -25,3 +25,26 @@ class OrderType(Enum):
 class TradeSide(Enum):
     OPEN = "open"
     CLOSE = "close"
+
+
+# %% TIMEFRAME
+
+
+class OHLCVTimeframe(Enum):
+    """
+    Enumeration of supported OHLCV timeframes.
+    Values correspond to ccxt standard timeframe strings.
+    """
+
+    ONE_MINUTE = "1m"
+    FIVE_MINUTES = "5m"
+    FIFTEEN_MINUTES = "15m"
+    THIRTY_MINUTES = "30m"
+    ONE_HOUR = "1h"
+    FOUR_HOURS = "4h"
+    ONE_DAY = "1d"
+    ONE_WEEK = "1w"
+    ONE_MONTH = "1M"
+
+    def __str__(self) -> str:
+        return self.value
